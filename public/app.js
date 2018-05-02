37107dc6da3e6b9018260fd65273cbbbb346ac21
Martin Sosa <mr.martinsosa@gmail.com>
2018-05-02 17:17:21 -0400

    updated layout

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 let closeButton = document.querySelector(".close-button")
 let flashPanel = document.querySelector(".flash")
+let navLink = document.querySelector(".nav-link")
 
 $('.carousel').carousel({
     interval: 6000
@@ -10,4 +11,8 @@ closeButton.addEventListener("click", event => {
   event.preventDefault()
 
   flashPanel.classList.add("hide")
+})
+
+navLink.addEventListener("click", event => {
+    
 })
